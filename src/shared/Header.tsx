e3e011e4e1c1745e76f6097128ff2688a9758fe1
Segura <segura-deluna@mail.ru>
2022-02-14 22:33:32 +0500

    make components header & day

---
+++ src/shared/Header.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Select from 'react-select';
+import { GlobalSvgSelector } from '../assets/icons/global/GlobalSvgSelector';
+import s from './Header.module.scss';
+
+export interface Props {
+}
+
+export const Header = (props: Props) => {
+	const options = [
+		{ value: 'city-1', label: 'Москва' },
+		{ value: 'city-2', label: 'Санкт-Петербург' },
+		{ value: 'city-3', label: 'Ноябрьск' }
+	]
+
+	const colorStyles = {
+		control: (styles: any) => ({
+			...styles,
+			backgroundColor: 'rgba(71, 147, 255, 0.2)',
+			width: '194px',
+			height: '37px',
+			border: 'none',
+			borderRadius: '10px',
+		})
+	}
+
+	return (
+		<header className={s.header}>
+			<div className={s.wrapper}>
+				<div className={s.logo}>
+					<GlobalSvgSelector id='header-logo' />
+				</div>
+				<div className={s.title}>React Weather</div>
+			</div>
+			<div className={s.wrapper}>
+				<div className={s.change_theme}>
+					<GlobalSvgSelector id='change-theme' />
+				</div>
+				<Select defaultValue={options[0]} styles={colorStyles} options={options} />
+			</div>
+		</header>
+	);
+}
